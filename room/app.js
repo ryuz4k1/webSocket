af08de6e6c0ef1712569e47d7b96d92c42c0f07c
ryuz4k1 <ozerr.murat@hotmail.com>
2019-12-01 22:50:57 +0300

    reconnection status added

--- room/app.js
+++ room/app.js
@@ -12,12 +12,23 @@ const io = socketio.listen(server);
 
 io.on('connection', (socket) => {
 
+    //console.log(socket.id);
+
+    socket.join("room1");
+    socket.join("room2");
+    socket.join("room3", () => {
+        const rooms = Object.keys(socket.rooms);
+        //console.log(rooms); //[ 'OExN0UI_oxI1A6kmAAAA', 'room1', 'room2', 'room3' ] Bu idnin bulunduğu odalar
+    });
+
     socket.on('joinRoom', (data) => {
         socket.join(data.name, () => {
             console.log('Birisi odaya girdi');
             //socket.to(data.name).emit('new'); //Ben haric bu odadaki herkese yollar herkese yollar
             io.to(data.name).emit('new', {count: getOnlineCount(io, data)}); //Benimle beraber bu odadaki herkese yollar.
             socket.emit('joinLog', {message: 'Odaya girdiniz'});
+            const rooms = Object.keys(socket.rooms);
+            console.log(rooms);
         });
     });
 
